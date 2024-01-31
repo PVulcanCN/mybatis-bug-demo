df468bcd74498de663a5956ddd677559371ac927
yongqin.peng <pengyongqin@lechengyunfu.com>
2024-01-31 17:29:41 +0800

    sql script

--- src/main/resources/sql/encrypt_test.sql
+++ src/main/resources/sql/encrypt_test.sql
@@ -11,6 +11,12 @@ CREATE TABLE `encrypt_test`  (
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
   `var` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 0 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1752616456007323649 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of encrypt_test
+-- ----------------------------
+INSERT INTO `encrypt_test` VALUES (1, 'aa', NULL, 'a');
+INSERT INTO `encrypt_test` VALUES (2, 'aa', NULL, 'b');
 
 SET FOREIGN_KEY_CHECKS = 1;
